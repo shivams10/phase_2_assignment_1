330581abf8057572822636c5cefd6f82b15a8089
Shivam Shukla <shivam@gkmit.co>
2022-11-21 14:35:56 +0530

    added project section using javascript

--- script.js
+++ script.js
@@ -2,6 +2,47 @@ const form = document.querySelector(".feedback-form");
 const input = document.querySelector(".feedback-input");
 const list = document.querySelector("#dynamic-list");
 
+const externalFile = "./projects.json";
+fetch(externalFile)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log("error: " + err);
+  });
+function appendData(data) {
+  const mainContainer = document.querySelector(".cards");
+  for (var i = 0; i < data.length; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    mainContainer.appendChild(card);
+
+    const cardTitle = document.createElement("h2");
+    cardTitle.classList.add("card-title");
+    cardTitle.innerHTML = data[i].title;
+    card.appendChild(cardTitle);
+
+    const image = document.createElement("img");
+    image.setAttribute("src",data[i].imageLink);
+    image.setAttribute("alt","project");
+    card.appendChild(image)
+
+    const cardDescription = document.createElement("p");
+    cardDescription.classList.add("card-desc");
+    cardDescription.innerHTML = data[i].description;
+    card.appendChild(cardDescription);
+
+    const github = document.createElement("a");
+    github.setAttribute("href",data[i].githubLink);
+    github.innerHTML = "Github Link>>";
+    cardDescription.appendChild(github);
+  }
+}
+
 window.addEventListener("load", () => onBrowserLoad());
 
 function onBrowserLoad() {
